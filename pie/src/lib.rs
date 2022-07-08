399e9030dbb2a6f7335982d4257708b5bab6a56d
Gabriel Konat <gabrielkonat@gmail.com>
2022-07-08 11:23:32 +0200

    WIP.

--- pie/src/lib.rs
+++ pie/src/lib.rs
@@ -1,12 +1,14 @@
 // Using Eq/PartialEq/Hash as trait objects: https://users.rust-lang.org/t/workaround-for-hash-trait-not-being-object-safe/53332/8 and https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=3a6d8b0a2e45ee2392b68f36c79d6173 and https://github.com/dtolnay/dyn-clone
 
 use std::any::Any;
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use std::hash::{Hash, Hasher};
 use std::io::Read;
 use std::path::PathBuf;
+use std::rc::Rc;
 use std::time::SystemTime;
 
 use anymap::AnyMap;
@@ -14,7 +16,7 @@ use dyn_clone::DynClone;
 
 // Task key
 
-trait DynEq {
+pub trait DynEq {
   fn dyn_eq(&self, other: &dyn Any) -> bool;
 }
 
@@ -29,7 +31,7 @@ impl<T: Eq + Any> DynEq for T {
   }
 }
 
-trait DynHash {
+pub trait DynHash {
   fn dyn_hash(&self, state: &mut dyn Hasher);
 }
 
@@ -78,11 +80,12 @@ impl<T: DynEq + DynHash + DynClone + 'static + ?Sized> TaskKey for T {}
 // Context
 
 pub struct Context {
-  current_task_key: Box<dyn TaskKey>,
+  current_task: Box<dyn DynTask>,
+  store: Rc<RefCell<Store>>,
 }
 
 impl Context {
-  fn new(current_task_key: Box<dyn TaskKey>) -> Self { Self { current_task_key } }
+  fn new(current_task: Box<dyn DynTask>, store: Rc<RefCell<Store>>) -> Self { Self { current_task, store } }
 
   pub fn require_task<T: Task>(&mut self, task: &T) -> Result<T::Output, Box<dyn Error>> {
     // TODO: create require dependency from current task to task.
@@ -100,26 +103,34 @@ impl Context {
 
 // Task + implementations
 
-pub trait Task: Clone {
-  type Key: Eq + Hash + Clone + 'static;
-  fn key(&self) -> &Self::Key;
-
+pub trait Task: DynTask + Eq + Hash + Clone + 'static {
   type Output: Eq + Clone + 'static;
   fn execute(&self, context: &mut Context) -> Self::Output;
 }
 
+pub trait DynTask: DynEq + DynHash + DynClone + 'static {}
+
+impl<T: Task> DynTask for T {}
+
+// Noop task
+
+#[derive(Default, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
+pub struct NoopTask {}
+
+impl Task for NoopTask {
+  type Output = ();
+  #[inline]
+  fn execute(&self, _context: &mut Context) -> Self::Output { () }
+}
+
 // Read file to string task
 
-#[derive(Clone)]
+#[derive(Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
 pub struct ReadFileToString {
   path: PathBuf,
 }
 
 impl Task for ReadFileToString {
-  type Key = PathBuf;
-  #[inline]
-  fn key(&self) -> &Self::Key { &self.path }
-
   // Use ErrorKind instead of Error which impls Eq and Clone.
   type Output = Result<String, std::io::ErrorKind>;
   #[inline]
@@ -158,7 +169,7 @@ impl<T: Task> TaskDependency<T> {
 impl<T: Task> Dependency for TaskDependency<T> {
   #[inline]
   fn is_consistent(&self, context: &mut Context, store: &mut Store) -> Result<bool, Box<dyn Error>> {
-    if let Some(previous_output) = store.get_task_output::<T>(self.task.key()) {
+    if let Some(previous_output) = store.get_task_output::<T>(&self.task) {
       let output = context.require_task::<T>(&self.task)?;
       return Ok(output == *previous_output);
     }
@@ -203,21 +214,30 @@ pub struct Store {
 
 impl Store {
   #[inline]
-  fn get_task_dependencies_map_mut<T: Task>(&mut self) -> &mut HashMap<T::Key, Vec<Box<dyn Dependency>>> {
-    self.task_dependencies.entry::<HashMap<T::Key, Vec<Box<dyn Dependency>>>>().or_insert_with(|| HashMap::default())
+  fn new() -> Self {
+    Self {
+      task_outputs: AnyMap::new(),
+      task_dependencies: AnyMap::new(),
+      file_modification_dates: HashMap::default()
+    }
   }
 
   #[inline]
-  fn get_task_output_map<T: Task>(&self) -> Option<&HashMap<T::Key, T::Output>> {
-    self.task_outputs.get::<HashMap<T::Key, T::Output>>()
+  fn get_task_dependencies_map_mut<T: Task>(&mut self) -> &mut HashMap<T, Vec<Box<dyn Dependency>>> {
+    self.task_dependencies.entry::<HashMap<T, Vec<Box<dyn Dependency>>>>().or_insert_with(|| HashMap::default())
+  }
+
+  #[inline]
+  fn get_task_output_map<T: Task>(&self) -> Option<&HashMap<T, T::Output>> {
+    self.task_outputs.get::<HashMap<T, T::Output>>()
   }
   #[inline]
-  fn get_task_output_map_mut<T: Task>(&mut self) -> &mut HashMap<T::Key, T::Output> {
-    self.task_outputs.entry::<HashMap<T::Key, T::Output>>().or_insert_with(|| HashMap::default())
+  fn get_task_output_map_mut<T: Task>(&mut self) -> &mut HashMap<T, T::Output> {
+    self.task_outputs.entry::<HashMap<T, T::Output>>().or_insert_with(|| HashMap::default())
   }
   #[inline]
-  fn get_task_output<T: Task>(&self, task_key: &T::Key) -> Option<&T::Output> {
-    self.get_task_output_map::<T>().map_or(None, |map| map.get(task_key))
+  fn get_task_output<T: Task>(&self, task: &T) -> Option<&T::Output> {
+    self.get_task_output_map::<T>().map_or(None, |map| map.get(task))
   }
 }
 
@@ -227,7 +247,7 @@ pub struct NaiveRunner {}
 impl NaiveRunner {
   #[inline]
   pub fn require_task<T: Task>(&mut self, task: &T) -> Result<T::Output, Box<dyn Error>> {
-    let mut context = Context::new(Box::new(()));
+    let mut context = Context::new(Box::new(task.clone()), Rc::new(RefCell::new(Store::new()))); // OPTO: create store once.
     Ok(task.execute(&mut context))
   }
 }
@@ -235,17 +255,17 @@ impl NaiveRunner {
 // Top-down incremental runner
 
 pub struct TopDownRunner {
-  store: Store,
+  store: Rc<RefCell<Store>>,
 }
 
 impl TopDownRunner {
   pub fn require_task<T: Task>(&mut self, task: &T) -> Result<T::Output, Box<dyn Error>> {
-    let mut context = Context::new(Box::new(task.key().clone()));
+    let mut context = Context::new(Box::new(task.clone()), self.store.clone());
     if self.should_execute_task(task, &mut context)? {
       TaskExecutor::execute(task, &mut context, &mut self.store)
     } else {
       // Assume: if we should not execute the task, it must have been executed before, and therefore it has an output.
-      let output = self.store.get_task_output::<T>(task.key()).unwrap().clone();
+      let output = self.store.borrow_mut().get_task_output::<T>(task).unwrap().clone();
       Ok(output)
     }
   }
@@ -253,17 +273,17 @@ impl TopDownRunner {
   fn should_execute_task<T: Task>(&mut self, task: &T, context: &mut Context) -> Result<bool, Box<dyn Error>> {
     // Remove task dependencies so that we get ownership over the list of dependencies. If the task does not need to be
     // executed, we will restore the dependencies again.
-    let task_dependencies = self.store.get_task_dependencies_map_mut::<T>().remove(task.key());
+    let task_dependencies = self.store.borrow_mut().get_task_dependencies_map_mut::<T>().remove(task);
     if let Some(task_dependencies) = task_dependencies {
       // Task has been executed before, check whether all its dependencies are still consistent. If one or more are not,
       // we need to execute the task.
       for task_dependency in &task_dependencies {
-        if !task_dependency.is_consistent(context, &mut self.store)? {
+        if !task_dependency.is_consistent(context, &mut self.store.borrow_mut())? {
           return Ok(true);
         }
       }
       // Task is consistent and does not need to be executed. Restore the previous dependencies.
-      self.store.get_task_dependencies_map_mut::<T>().insert(task.key().clone(), task_dependencies); // OPTO: removing and inserting into a HashMap due to ownership requirements.
+      self.store.borrow_mut().get_task_dependencies_map_mut::<T>().insert(task.clone(), task_dependencies); // OPTO: removing and inserting into a HashMap due to ownership requirements.
       Ok(false)
     } else {
       // Task has not been executed before, therefore we need to execute it.
@@ -277,7 +297,7 @@ impl TopDownRunner {
 pub struct TaskExecutor {}
 
 impl TaskExecutor {
-  fn execute<T: Task>(task: &T, context: &mut Context, store: &mut Store) -> Result<T::Output, Box<dyn Error>> {
+  fn execute<T: Task>(task: &T, context: &mut Context, store: &RefCell<Store>) -> Result<T::Output, Box<dyn Error>> {
     let output = task.execute(context);
     // TODO: store dependencies that the task made!
     // TODO: store output of the task!
