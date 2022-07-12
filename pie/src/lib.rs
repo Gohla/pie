88472b261decf4ba9d2eea0fd686b05aae772013
Gabriel Konat <gabrielkonat@gmail.com>
2022-07-12 12:57:09 +0200

    Cycle check and cleanup.

--- pie/src/lib.rs
+++ pie/src/lib.rs
@@ -1,4 +1,6 @@
-// Using Eq/PartialEq/Hash as trait objects: https://users.rust-lang.org/t/workaround-for-hash-trait-not-being-object-safe/53332/8 and https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=3a6d8b0a2e45ee2392b68f36c79d6173 and https://github.com/dtolnay/dyn-clone
+// Using Eq/PartialEq/Hash as trait objects: https://users.rust-lang.org/t/workaround-for-hash-trait-not-being-object-safe/53332/8 
+// and https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=3a6d8b0a2e45ee2392b68f36c79d6173 
+// and https://github.com/dtolnay/dyn-clone
 
 use std::any::Any;
 use std::collections::HashMap;
@@ -11,6 +13,7 @@ use std::time::SystemTime;
 
 use anymap::AnyMap;
 use dyn_clone::{clone_box, DynClone};
+use hashlink::LinkedHashSet;
 
 // Trait object helpers
 
@@ -174,58 +177,6 @@ impl<C: Context> Dependency<C> for FileDependency {
 }
 
 
-// Store
-
-pub struct Store<C: Context> {
-  task_outputs: AnyMap,
-  task_dependencies: HashMap<Box<dyn DynTask>, Vec<Box<dyn Dependency<C>>>>,
-}
-
-impl<C: Context> Store<C> {
-  #[inline]
-  fn new() -> Self {
-    Self {
-      task_outputs: AnyMap::new(),
-      task_dependencies: HashMap::default(),
-    }
-  }
-
-  #[inline]
-  fn remove_task_dependencies(&mut self, task: &dyn DynTask) -> Option<Vec<Box<dyn Dependency<C>>>> {
-    self.task_dependencies.remove(task)
-  }
-  #[inline]
-  fn set_task_dependencies(&mut self, task: Box<dyn DynTask>, dependencies: Vec<Box<dyn Dependency<C>>>) {
-    self.task_dependencies.insert(task, dependencies);
-  }
-  #[inline]
-  fn add_to_task_dependencies(&mut self, task: Box<dyn DynTask>, dependency: Box<dyn Dependency<C>>) {
-    let dependencies = self.task_dependencies.entry(task).or_insert_with(|| Vec::new());
-    dependencies.push(dependency);
-  }
-
-  #[inline]
-  fn get_task_output_map<T: Task>(&self) -> Option<&HashMap<T, T::Output>> {
-    self.task_outputs.get::<HashMap<T, T::Output>>()
-  }
-  #[inline]
-  fn get_task_output_map_mut<T: Task>(&mut self) -> &mut HashMap<T, T::Output> {
-    self.task_outputs.entry::<HashMap<T, T::Output>>().or_insert_with(|| HashMap::default())
-  }
-  #[inline]
-  fn get_task_output<T: Task>(&self, task: &T) -> Option<&T::Output> {
-    self.get_task_output_map::<T>().map_or(None, |map| map.get(task))
-  }
-  #[inline]
-  fn set_task_output<T: Task>(&mut self, task: T, output: T::Output) {
-    self.get_task_output_map_mut::<T>().insert(task, output);
-  }
-}
-
-
-// Runners
-
-
 // Naive runner
 
 pub struct NaiveRunner {}
@@ -249,24 +200,27 @@ impl Context for NaiveRunner {
 // Top-down incremental runner
 
 pub struct TopDownRunner {
-  store: Store<Self>,
-  task_execution_stack: Vec<Box<dyn DynTask>>,
+  task_outputs: AnyMap,
+  task_dependencies: HashMap<Box<dyn DynTask>, Vec<Box<dyn Dependency<Self>>>>,
+  task_execution_stack: LinkedHashSet<Box<dyn DynTask>>,
 }
 
 impl Context for TopDownRunner {
   fn require_task<T: Task>(&mut self, task: &T) -> Result<T::Output, Box<dyn Error>> {
     if self.should_execute_task(task)? {
-      self.task_execution_stack.push(Box::new(task.clone())); // TODO: check for cycles with LinkedHashSet!
-      let output = task.execute(self);
-      self.task_execution_stack.pop();
-      if let Some(current_task) = self.task_execution_stack.last() {
-        self.store.add_to_task_dependencies(current_task.clone(), Box::new(TaskDependency::new(task.clone(), output.clone())));
+      if !self.task_execution_stack.insert(Box::new(task.clone())) {
+        panic!("Cycle");
       }
-      self.store.set_task_output(task.clone(), output.clone());
+      let output = task.execute(self);
+      self.task_execution_stack.pop_back();
+      if let Some(current_task) = self.task_execution_stack.back() {
+        self.add_to_task_dependencies(current_task.clone(), Box::new(TaskDependency::new(task.clone(), output.clone())));
+      }
+      self.set_task_output(task.clone(), output.clone());
       Ok(output)
     } else {
       // Assume: if we should not execute the task, it must have been executed before, and therefore it has an output.
-      let output = self.store.get_task_output::<T>(task).unwrap().clone();
+      let output = self.get_task_output::<T>(task).unwrap().clone();
       Ok(output)
     }
   }
@@ -274,8 +228,8 @@ impl Context for TopDownRunner {
   fn require_file(&mut self, path: &PathBuf) -> Result<File, std::io::ErrorKind> { // TODO: hidden dependency detection
     let dependency = FileDependency::new(path.clone()).map_err(|e| e.kind())?;
     let opened = dependency.open();
-    if let Some(current_task) = self.task_execution_stack.last() {
-      self.store.add_to_task_dependencies(current_task.clone(), Box::new(dependency));
+    if let Some(current_task) = self.task_execution_stack.back() {
+      self.add_to_task_dependencies(current_task.clone(), Box::new(dependency));
     }
     opened
   }
@@ -283,25 +237,26 @@ impl Context for TopDownRunner {
   fn provide_file(&mut self, path: &PathBuf) -> Result<File, std::io::ErrorKind> { // TODO: hidden dependency detection
     let dependency = FileDependency::new(path.clone()).map_err(|e| e.kind())?;
     let opened = dependency.open();
-    if let Some(current_task) = self.task_execution_stack.last() {
-      self.store.add_to_task_dependencies(current_task.clone(), Box::new(dependency));
+    if let Some(current_task) = self.task_execution_stack.back() {
+      self.add_to_task_dependencies(current_task.clone(), Box::new(dependency));
     }
     opened
   }
 }
 
 impl TopDownRunner {
-  fn new() -> Self {
+  pub fn new() -> Self {
     Self {
-      store: Store::new(),
-      task_execution_stack: Vec::new(),
+      task_outputs: AnyMap::new(),
+      task_dependencies: HashMap::new(),
+      task_execution_stack: LinkedHashSet::new(),
     }
   }
 
   fn should_execute_task(&mut self, task: &dyn DynTask) -> Result<bool, Box<dyn Error>> {
     // Remove task dependencies so that we get ownership over the list of dependencies. If the task does not need to be
     // executed, we will restore the dependencies again.
-    let task_dependencies = self.store.remove_task_dependencies(task);
+    let task_dependencies = self.remove_task_dependencies(task);
     if let Some(task_dependencies) = task_dependencies {
       // Task has been executed before, check whether all its dependencies are still consistent. If one or more are not,
       // we need to execute the task.
@@ -311,12 +266,43 @@ impl TopDownRunner {
         }
       }
       // Task is consistent and does not need to be executed. Restore the previous dependencies.
-      self.store.set_task_dependencies(clone_box(task), task_dependencies); // OPTO: removing and inserting into a HashMap due to ownership requirements.
+      self.set_task_dependencies(clone_box(task), task_dependencies); // OPTO: removing and inserting into a HashMap due to ownership requirements.
       Ok(false)
     } else {
       // Task has not been executed before, therefore we need to execute it.
       Ok(true)
     }
+  }
+
+  #[inline]
+  fn remove_task_dependencies(&mut self, task: &dyn DynTask) -> Option<Vec<Box<dyn Dependency<Self>>>> {
+    self.task_dependencies.remove(task)
+  }
+  #[inline]
+  fn set_task_dependencies(&mut self, task: Box<dyn DynTask>, dependencies: Vec<Box<dyn Dependency<Self>>>) {
+    self.task_dependencies.insert(task, dependencies);
+  }
+  #[inline]
+  fn add_to_task_dependencies(&mut self, task: Box<dyn DynTask>, dependency: Box<dyn Dependency<Self>>) {
+    let dependencies = self.task_dependencies.entry(task).or_insert_with(|| Vec::new());
+    dependencies.push(dependency);
+  }
+
+  #[inline]
+  fn get_task_output_map<T: Task>(&self) -> Option<&HashMap<T, T::Output>> {
+    self.task_outputs.get::<HashMap<T, T::Output>>()
+  }
+  #[inline]
+  fn get_task_output_map_mut<T: Task>(&mut self) -> &mut HashMap<T, T::Output> {
+    self.task_outputs.entry::<HashMap<T, T::Output>>().or_insert_with(|| HashMap::default())
+  }
+  #[inline]
+  fn get_task_output<T: Task>(&self, task: &T) -> Option<&T::Output> {
+    self.get_task_output_map::<T>().map_or(None, |map| map.get(task))
+  }
+  #[inline]
+  fn set_task_output<T: Task>(&mut self, task: T, output: T::Output) {
+    self.get_task_output_map_mut::<T>().insert(task, output);
   }
 }
 
@@ -325,7 +311,7 @@ mod test {
   use std::fs;
   use std::path::PathBuf;
 
-  use crate::{Context, ReadFileToString, TopDownRunner};
+  use crate::{Context, ReadFileToString, Task, TopDownRunner};
 
   #[test]
   fn test() {
@@ -336,5 +322,20 @@ mod test {
     let task = ReadFileToString::new(path);
     runner.require_task(&task).unwrap().unwrap();
     runner.require_task(&task).unwrap().unwrap();
+  }
+
+  #[test]
+  #[should_panic(expected = "Cycle")]
+  fn cycle_panics() {
+    let mut runner = TopDownRunner::new();
+    #[derive(Clone, PartialEq, Eq, Hash)]
+    struct RequireSelf;
+    impl Task for RequireSelf {
+      type Output = ();
+      fn execute<C: Context>(&self, context: &mut C) -> Self::Output {
+        context.require_task(self).unwrap();
+      }
+    }
+    runner.require_task(&RequireSelf).unwrap();
   }
 }
