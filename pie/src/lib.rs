18f48c864adfaf35056c81a85010f9792cc2b565
Gabriel Konat <gabrielkonat@gmail.com>
2022-09-02 09:59:17 +0200

    Minor doc fix.

--- pie/src/lib.rs
+++ pie/src/lib.rs
@@ -135,12 +135,12 @@ impl<'p, A, H, C> Drop for Session<'p, A, H, C> {
 /// Incremental context, mediating between tasks and runners, enabling tasks to dynamically create dependencies that 
 /// runners check for consistency and use in incremental execution.
 pub trait Context {
-  /// Requires given `[task]`, creating a dependency to it, and returning its up-to-date output.
+  /// Requires given `task`, creating a dependency to it, and returning its up-to-date output.
   fn require_task<T: Task>(&mut self, task: &T) -> T::Output;
-  /// Requires file at given `[path]`, creating a read-dependency to the file by reading its content or metadata at the 
+  /// Requires file at given `path`, creating a read-dependency to the file by reading its content or metadata at the 
   /// time this function is called, and returning the opened file. Call this method *before reading from the file*.
   fn require_file(&mut self, path: &PathBuf) -> Result<File, std::io::Error>;
-  /// Provides file at given `[path]`, creating a write-dependency to it by writing to its content or changing its
+  /// Provides file at given `path`, creating a write-dependency to it by writing to its content or changing its
   /// metadata at the time this function is called. Call this method *after writing to the file*. This method does not 
   /// return the opened file, as it must be called *after writing to the file*.
   fn provide_file(&mut self, path: &PathBuf) -> Result<(), std::io::Error>;
