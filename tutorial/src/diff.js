3fb048917e060d38af217aec2120045b5f23c664
Gabriel Konat <gabrielkonat@gmail.com>
2023-03-24 14:28:43 +0100

    Fix lines without diff being misaligned.

--- tutorial/src/diff.js
+++ tutorial/src/diff.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", (_) => {
   const codeClassPattern = /.*customdiff.*/;
   const diffLinePattern = /^(?<span><\/span>)?(?<diff>[-+])(?<rest>.*)/;
+  const nonDiffLinePattern = /^ /;
   document.querySelectorAll("code").forEach((el) => {
     if (codeClassPattern.test(el.className)) {
       const lines = el.innerHTML.split("\n").map(line => {
         const match = line.match(diffLinePattern);
         if (!match) {
-          return line;
+          return line.replace(nonDiffLinePattern, "");
         } else {
           const removal = match.groups.diff === "-";
           return `${match.groups.span ? `</span>` : ``}<span style="background-color: ${removal ? `#ffeef0` : `#f0fff4`}; display: inline-block; width: 100%;">${match.groups.rest}</span>`;
