98c6d4a7f1f7ef67c9d839d084bf6c0ba299d3f2
Gabriel Konat <gabrielkonat@gmail.com>
2023-07-17 16:45:55 +0200

    WIP mdbook treesitter table syntax highlighter.

---
+++ tutorial/mdbook-treesitter-table-highlight/src/main.rs
@@ -0,0 +1,67 @@
+use std::io;
+use std::process;
+
+use anyhow::Result;
+use clap::{Arg, ArgMatches, Command};
+use mdbook::preprocess::{CmdPreprocessor, Preprocessor};
+use semver::{Version, VersionReq};
+
+use mdbook_treesitter_table_highlight::TreesitterTableHighlight;
+
+pub fn make_app() -> Command {
+  Command::new("nop-preprocessor")
+    .about("A mdbook preprocessor which does precisely nothing")
+    .subcommand(
+      Command::new("supports")
+        .arg(Arg::new("renderer").required(true))
+        .about("Check whether a renderer is supported by this preprocessor"),
+    )
+}
+
+fn main() {
+  let matches = make_app().get_matches();
+
+  let preprocessor = TreesitterTableHighlight::new().unwrap();
+
+  if let Some(sub_args) = matches.subcommand_matches("supports") {
+    handle_supports(&preprocessor, sub_args);
+  } else if let Err(e) = handle_preprocessing(&preprocessor) {
+    eprintln!("{}", e);
+    process::exit(1);
+  }
+}
+
+fn handle_preprocessing(pre: &dyn Preprocessor) -> Result<()> {
+  let (ctx, book) = CmdPreprocessor::parse_input(io::stdin())?;
+
+  let book_version = Version::parse(&ctx.mdbook_version)?;
+  let version_req = VersionReq::parse(mdbook::MDBOOK_VERSION)?;
+
+  if !version_req.matches(&book_version) {
+    eprintln!(
+      "Warning: The {} plugin was built against version {} of mdbook, but we're being called from version {}",
+      pre.name(),
+      mdbook::MDBOOK_VERSION,
+      ctx.mdbook_version
+    );
+  }
+
+  let processed_book = pre.run(&ctx, book)?;
+  serde_json::to_writer(io::stdout(), &processed_book)?;
+
+  Ok(())
+}
+
+fn handle_supports(pre: &dyn Preprocessor, sub_args: &ArgMatches) -> ! {
+  let renderer = sub_args
+    .get_one::<String>("renderer")
+    .expect("Required argument");
+  let supported = pre.supports_renderer(renderer);
+
+  // Signal whether the renderer is supported by exiting with 1 or 0.
+  if supported {
+    process::exit(0);
+  } else {
+    process::exit(1);
+  }
+}
